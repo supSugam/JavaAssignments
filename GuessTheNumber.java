b1918d1c4c928a2542b1f9d90fea209f466d5bff
supSugam <sugamsubedi234@gmail.com>
2022-06-27 11:12:53 +0545

    Updated Program with Proper Exception Handling

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -4,39 +4,41 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumber {
-    // Generate a random number
 
     public static void main(String[] args) {
 
         Random random = new Random();
         int n = random.nextInt(10);
 
-        Scanner input = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
         int counter = 0;
         int num = 0;
-
-        System.out.println("Guess the Number from 0-10!");
         try {
-            num = input.nextInt();
-        } catch (Exception e){
-            System.out.println("Invalid Value Entered!");
-        }
-        System.out.println("Invalid Input Entered!");
+            System.out.println("Guess the Number from 0-10!");
+            num = scan.nextInt();
 
-        while (num!=n) {
-            System.out.println("Enter Value Again.");
+        } catch (InputMismatchException exception) {
+            System.out.println("Invalid Type of Input!");
+            counter++;
+        }
+
+        while (num != n) {
             try {
-                num = input.nextInt();
+
+                System.out.println("Enter the Number Again!!");
+                num = scanner.nextInt();
                 counter++;
-            } catch (InputMismatchException ex){
-                input.nextLine();
-                System.out.println("Invalid Value Entered!");
-                System.out.println("Total Incorrect Attempts: "+counter);
+
+            } catch (InputMismatchException ex) {
+                scan.nextLine();
+                System.out.println("Invalid Type of Input!");
+                counter++;
             }
         }
+
         System.out.println("The number is correct.");
-        System.out.println("Total Incorrect Attempts: "+counter);
-
+        System.out.println("Total Incorrect Attempts: " + counter);
     }
-
 }
